9355e926a6808f04400d92882100c243763fc037
Silvio Ankermann <silvio@booq.org>
2021-02-23 19:00:55 +0100

    Fix logging

--- csi/utils.py
+++ csi/utils.py
@@ -7,7 +7,7 @@ def log_request_and_reply(f: callable=None, fields: list[str]=[]):
         def logging_wrapper(self, *args, **kwargs):
             self.logger.debug("Received request: {}".format(f.__name__))
             for field in fields:
-                self.logger.debug("{}: {}".format(field, getattr(args['request'], field, "[not found]")))
+                self.logger.debug("{}: {}".format(field, getattr(args[0], field, "[not found]")))
             reply = f(self, *args, **kwargs)
             self.logger.debug("Replying: {}".format(reply))
             return reply
