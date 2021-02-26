89e742f9feaa905c3990d6af24c252eaf2940b54
Silvio Ankermann <silvio@booq.org>
2021-02-26 14:45:53 +0100

    Always log whole request

--- csi/utils.py
+++ csi/utils.py
@@ -5,11 +5,11 @@ def log_request_and_reply(f: callable=None, fields: list[str]=[]):
     def _decorate(f):
         @wraps(f)
         def logging_wrapper(self, *args, **kwargs):
-            self.logger.debug("Received request: {}".format(f.__name__))
-            for field in fields:
-                self.logger.debug("{}: {}".format(field, getattr(args[0], field, "[not found]")))
+            self.logger.debug(f"Received request: {f.__name__}\n{args[0]}")
+            # for field in fields:
+            #     self.logger.debug("{}: {}".format(field, getattr(args[0], field, "[not found]")))
             reply = f(self, *args, **kwargs)
-            self.logger.debug("Replying: {}".format(reply))
+            self.logger.debug(f"Replying:\n{reply}")
             return reply
 
         return logging_wrapper
